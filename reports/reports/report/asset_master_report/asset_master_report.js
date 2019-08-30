86b2a477a806e48eb5225159bc9c1028487ff135
sammish <sammish.thundiyil@gmail.com>
2019-08-30 17:16:36 +0000

    Asset Report

---
+++ reports/reports/report/asset_master_report/asset_master_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, sammish.thundiyil@gmail.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Asset Master Report"] = {
+	"filters": [
+
+	]
+};
